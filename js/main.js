5ecae9c0f8ee194af5872191ab91ce7f6e87a4dd
shadyhag <misterwhiskers16@gmail.com>
2016-04-27 06:21:13 -0400

    figuring out dialog box

--- js/main.js
+++ js/main.js
@@ -1,15 +1,35 @@
-/*
-http://www.chartjs.org/
-
-===================== */
 
 window.onload = function() {
+
 /*=====================
 
 DATA
 
 ===================== */
-var citymarkers = "https://shayda.cartodb.com/api/v2/sql?format=GeoJSON&q=SELECT * FROM pennjobsdatatable20160418";
+// var citymarkers = "https://shayda.cartodb.com/api/v2/sql?format=GeoJSON&q=SELECT * FROM pennjobsdatatable20160418";
+
+
+
+var cartoDBUsername = "shayda";
+
+// Write SQL Selection Query to be Used on CartoDB Table
+// Name of table is 'data_collector'
+var sqlQuery = "SELECT * FROM pennjobsdatatable20160418";
+// Get CartoDB selection as GeoJSON and Add to Map
+function getGeoJSON(){
+  $.getJSON("https://"+cartoDBUsername+".cartodb.com/api/v2/sql?format=GeoJSON&q="+sqlQuery, function(data) {
+    cartoDBPoints = L.geoJson(data,{
+      pointToLayer: function(feature,loc){
+        return L.circleMarker(loc, cityMarkerOptions).bindPopup('<p>' + feature.properties.description + '<br /><em>Submitted by </em>' + feature.properties.name + '</p>');
+      },
+      onEachFeature: onEachFeature
+    }).addTo(map);
+  });
+}
+
+$( document ).ready(function() {
+  getGeoJSON();
+});
 
 var tableName = "pennjobsdatatable20160418";
 
@@ -21,6 +41,8 @@ var layerSource = {
     cartocss: cityMarkerOptions
   }]
 };
+
+
 
 
 /*=====================
@@ -44,17 +66,6 @@ var cityMarkerOptions = {
 
 
 
-var simpleStyle = '#pennjobsdatatable20160418 {' +
-'marker-fill-opacity: 0.9;' +
-'marker-line-color: #FFF;' +
-'marker-line-width: 1.5;' +
-'marker-line-opacity: 1;' +
-'marker-placement: point;' +
-'marker-type: ellipse;' +
-'marker-width: 10;' +
-'marker-fill: #FF6600;' +
-'marker-allow-overlap: true;' +
-'}';
 
 /*=====================
 
@@ -62,8 +73,6 @@ MARKER INTERACTIVITY
 
 ===================== */
 
-simpleStyle =  $("#simple").text();
-sublayer.setCartoCSS(simpleStyle);
 
 function highlightFeature(e) {
   var layer = e.target;
@@ -113,13 +122,53 @@ function onEachFeature(feature, layer) {
 /*=====================
 LAYER STUFF
 ===================== */
+var addLayer= function(url, options){
+  $.ajax(url).done(function(data){
+    console.log(url, data);
+    var geoJson = L.geoJson(data, options).addTo(map);
 
-addLayer(citymarkers,{
-  pointToLayer: function (feature, loc){
-    return L.circleMarker(loc, cityMarkerOptions);
-  },
-  onEachFeature: onEachFeature
-});
+  });
+
+};
+
+// addLayer(cityMa,{
+//   pointToLayer: function (feature, loc){
+//     return L.circleMarker(loc, cityMarkerOptions);
+//   },
+//   onEachFeature: onEachFeature
+// });
+
+
+// Create layer selector
+function createSelector(layer) {
+    var condition = "";
+    var $options = $(".layer_selector").find("li");
+    $options.click(function(e) {
+        var $li = $(e.target);
+        var selected = $li.attr('data');
+        var type = $li.data('type');
+
+        if (type === "cartocss") {
+            $options.removeClass('cartocss_selected');
+            if (selected !== "simple") {
+                $li.addClass('cartocss_selected');
+            }
+            condition = $('#'+selected).text();
+            layer.setCartoCSS(condition);
+        } else {
+            $options.removeClass('sql_selected');
+            if (selected !== "") {
+                $li.addClass('sql_selected');
+            }
+            if (selected.indexOf('guinea') !== -1) {
+                map_object.setView(L.latLng([-9.5, 147.116667]),6);
+            } else {
+                map_object.setView(L.latLng([37.7741154,-122.4437914]),2);
+            }
+            layer.setSQL("SELECT * FROM " + tableName + selected);
+        }
+    });
+}
 
 /*=====================
 DROPDOWN STUFF
@@ -143,6 +192,20 @@ var map = L.map('map', {
   center: [30.566174, -60.867132],
   zoom: 3
 });
+// for storing sublayer outside of createlayer
+var sublayers;
+
+// Add data layer to your map
+cartodb.createLayer(map,layerSource)
+    .addTo(map)
+    .done(function(layer) {
+        sublayer = layer.getSubLayer(0);
+        createSelector(sublayer);
+    })
+    .error(function(err) {
+        console.log("error: " + err);
+    });
+
 var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
   attribution: 'Map tiles by <State`="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   subdomains: 'abcd',
@@ -150,6 +213,4 @@ var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{
   maxZoom: 20,
   ext: 'png'
 }).addTo(map);
-
-
 };
