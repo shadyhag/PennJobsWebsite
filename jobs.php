e797c7f78b310655352311b090e8444dcd60dfac
shadyhag <misterwhiskers16@gmail.com>
2016-05-04 13:05:50 -0700

    add jobs.php

---
+++ jobs.php
@@ -0,0 +1,31 @@
+<?php
+define('BASE_URL', "https://shayda.cartodb.com/api/v2/sql?format=GeoJSON&api_key=f816e999bf9b5e7d7a5352138f2f23007fc184d4");
+
+function getJobs($exclude_types, $exclude_experience_levels){
+  $sql = "SELECT * FROM pennjobsdatatable20160220";
+  $wheres = [];
+
+
+  // exclude based on job_type field
+  if(!empty($exclude_types)){
+    $wheres[] = "job_type NOT IN('".implode($exclude_types, "','")."')";
+  }
+
+  // exclude based on the experience field
+  if(!empty($exclude_experience_levels)){
+    $wheres[] = "experience NOT IN('".implode($exclude_experience_levels, "','")."')";
+  }
+
+  // put together where clauses to make full query
+  if(!empty($wheres)){
+    $sql .= " WHERE ".implode(" AND ", $wheres);
+  }
+
+  $url = BASE_URL."&q=".urlencode($sql);
+  error_log("cartodb url: $url");
+
+  return file_get_contents($url);
+}
+
+$job_json = getJobs($_GET["not_types"], $_GET["not_experience"]);
+print($job_json);
