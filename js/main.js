7021f128f3d0b66f75d0e01ee70cfb9d8fa86ca5
shadyhag <misterwhiskers16@gmail.com>
2016-05-06 18:59:53 -0400

    Added SQL queries checkbox IP

--- js/main.js
+++ js/main.js
@@ -1,195 +1,239 @@
 
 window.onload = function() {
 
-/*=====================
+  /*=====================
 
-DATA RETRIEVAL AND SETUP
+  DATA RETRIEVAL AND SETUP
 
-===================== */
-var all = "https://shayda.cartodb.com/api/v2/sql?format=GeoJSON&q=SELECT * FROM pennjobsdatatable20160418";
+  ===================== */
 
+  var jobs = null;
 
-// cartodb.createVis(map, vizjson_url)
-//   .done(function(vis, layers) {
-//     // do stuff
-//     alert("Layers has " + layers.length + " layers.");
-//   })
-//   .error(function(err) {
-//     // report error
-//     console.log("An error occurred: " + err);
-//   });
+  var cartoDBUsername = "shayda";
+
+  // Write SQL Selection Query to be Used on CartoDB Table
+  // Name of table is 'data_collector'
 
 
+    /*=====================
 
-var jobs = null;
+    LOAD MAP WITH ALL JOBS
 
+    ===================== */
 
-var cartoDBUsername = "shayda";
+  var filterOptions = {
+    not_types: []
+  };
+  function showJobs(){
 
-// Write SQL Selection Query to be Used on CartoDB Table
-// Name of table is 'data_collector'
-var sqlQuery = "SELECT * FROM pennjobsdatatable20160418";
-var sqlQueryArchitecture = "SELECT * FROM pennjobsdatatable20160418 WHERE category LIKE 'Architecture'";
+    $.getJSON("http://shayda.rocks/jobs.php", filterOptions, function(data) {
+      jobs = L.geoJson(data,{
+        pointToLayer: function(feature,loc){
+          return L.circleMarker(loc, cityMarkerOptions).bindPopup('<p><b>' + feature.properties.name + '</b><br /><em>' + feature.properties.address + '</em></p>');
+        },
+        onEachFeature: onEachFeature
+      }).addTo(map);
+    });
+  }
 
-
-
-
-function showAll(){
-  $.getJSON("https://"+cartoDBUsername+".cartodb.com/api/v2/sql?format=GeoJSON&q="+sqlQuery, function(data) {
-    jobs = L.geoJson(data,{
-      pointToLayer: function(feature,loc){
-        return L.circleMarker(loc, cityMarkerOptions).bindPopup('<p><b>' + feature.properties.name + '</b><br /><em>' + feature.properties.address + '</em></p>');
-      },
-      onEachFeature: onEachFeature
-    }).addTo(map);
+  $( document ).ready(function() {
+    showJobs();
   });
-}
 
 
-showAll();
+  /*=====================
+  CHECKBOX STUFF
+  ===================== */
+
+  // $.get("jobs.php", {not_types: ["Unpaid", "Part-time", "Full-time", "Internship","Entry-level",
+  // "Senior-level", "Closed"]}, function(data) { console.log(data);});
+  //
+// }
+  // Event Listeners
 
 
-
-/*=====================
-DROPDOWN STUFF
-===================== */
-
-$('#sql .dropdown-menu a').click(function(e){
-  e.preventDefault();
-  var selection = $(this);
-  var selectedCategory = selection.attr('value');
-  var selectedSql = selection.data('sql');
-  $('#text-category').text(selectedCategory);
-  $('#text-sql').text(selectedSql);
-});
+  $('input[value=NoFT]').click(function(){
+    if($(this).checked()){
+      filterOptions["not_types"].push("Full-time");
+    }else{
+      filterOptions["not_types"].remove("Full-time");
+    }
+    showJobs();
+  });
 
 
-
-
-var tableName = "pennjobsdatatable20160418";
-
-var layerSource = {
-  user_name: 'shayda',
-  type: 'cartodb',
-  sublayers: [{
-    sql: "SELECT * FROM " + tableName, // Earthquakes from the past 30 days
-    cartocss: cityMarkerOptions
-  }]
-};
-
-
-/*=====================
-
-STYLES
-
-===================== */
+  // // Event Listeners
+  // $('input[value=all]').click(function(){
+  //   showAll();
+  //   console.log(jobs);
+  // });
 
 
 
-var cityMarkerOptions = {
-  radius: 8,
-  fillColor: "grey",
-  color: "grey",
-  weight: 1,
-  opacity: 0.5,
-  fillOpacity: 0.8,
-  onEachFeature: onEachFeature
+  /*=====================
+  DROPDOWN STUFF
+  ===================== */
 
-};
-
-
-
-
-/*=====================
-
-MARKER INTERACTIVITY
-
-===================== */
+  $('#sql .dropdown-menu a').click(function(e){
+    e.preventDefault();
+    var selection = $(this);
+    var selectedCategory = selection.attr('value');
+    var selectedSql = selection.data('sql');
+    $('#text-category').text(selectedCategory);
+    $('#text-sql').text(selectedSql);
+  });
 
 
-function highlightFeature(e) {
-  var layer = e.target;
 
-  layer.setStyle({
+  /*=====================
+
+  STYLES
+
+  ===================== */
+
+
+
+  var cityMarkerOptions = {
     radius: 8,
+    fillColor: "grey",
+    color: "grey",
     weight: 1,
-    fillColor: "#16174f",
-    color: "#16174f",
-    dashArray: '',
-    fillOpacity: 1
+    opacity: 0.5,
+    fillOpacity: 0.8,
+    onEachFeature: onEachFeature
 
+  };
+
+
+
+
+  /*=====================
+
+  MARKER INTERACTIVITY
+
+  ===================== */
+
+
+  function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+      radius: 8,
+      weight: 1,
+      fillColor: "#16174f",
+      color: "#16174f",
+      dashArray: '',
+      fillOpacity: 1
+
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera) {
+      layer.bringToFront();
+    }
+  }
+
+
+
+  function resetHighlight(e) {
+    var layer = e.target;
+
+    layer.setStyle(cityMarkerOptions);
+
+    if (!L.Browser.ie && !L.Browser.opera) {
+      layer.bringToFront();
+    }
+  }
+
+
+  function onEachFeature(feature, layer) {
+    layer.on({
+      mouseover: highlightFeature,
+      mouseout: resetHighlight,
+
+    });
+
+
+  }
+
+
+
+
+
+
+  /*=====================
+  LAYER STUFF
+  ===================== */
+
+
+
+
+
+
+
+  /* =====================
+  Leaflet Configuration
+  ===================== */
+
+  var map = L.map('map', {
+    center: [30.566174, -60.867132],
+    zoom: 3
   });
 
-  if (!L.Browser.ie && !L.Browser.opera) {
-    layer.bringToFront();
-  }
-}
+  /*=====================
 
+  Infowindow
 
+  ===================== */
 
-function resetHighlight(e) {
-  var layer = e.target;
+  // cartodb.createLayer(map, '"https://shayda.cartodb.com/api/v2/sql?format=GeoJSON&q=', {
+  //     query: 'SELECT * FROM pennjobsdatatable20160418 ORDER BY post_date DESC'
+  //
+  //   })
+  //     .on('done', function(layer) {
+  //
+  //       map.overlayMapTypes.setAt(1, layer);
+  //       var sublayer = layer.getSubLayer(0);
+  //       sublayer.setCartoCSS(systemcartoCSS);
+  //
+  //       layers.SystemLyr = sublayer;
+  //       var infowindow = sublayer.infowindow;
+  //
+  //       infowindow.set('template', function(data) {
+  //
+  //         var clickPosLatLng = this.model.get('latlng');
+  //         var fields = this.model.get('content').fields;
+  //
+  //         if (fields && fields[0].type !== 'loading') {
+  //
+  //           var obj = _.find(fields, function(obj) {
+  //             return obj.title == 'kml_key';
+  //           }).value;
+  //
+  //           callinfowindow(clickPosLatLng, obj);
+  //
+  //         }
+  //       }); // end infowindow set
+  //
+  //     }); // end on.done for the water systems
+  // // for storing sublayer outside of createlayer
+  // var sublayers;
+  //
+  // // // Add data layer to your map
+  // // cartodb.createLayer(map,layerSource)
+  // //     .addTo(map)
+  // //     .done(function(layer) {
+  // //         sublayer = layer.getSubLayer(0);
+  // //         createSelector(sublayer);
+  // //     })
+  // //     .error(function(err) {
+  // //         console.log("error: " + err);
+  // //     });
 
-  layer.setStyle(cityMarkerOptions);
-
-  if (!L.Browser.ie && !L.Browser.opera) {
-    layer.bringToFront();
-  }
-}
-
-
-function onEachFeature(feature, layer) {
-  layer.on({
-    mouseover: highlightFeature,
-    mouseout: resetHighlight,
-
-  });
-
-
-}
-
-
-
-
-
-
-/*=====================
-LAYER STUFF
-===================== */
-
-
-
-
-
-
-
-/* =====================
-Leaflet Configuration
-===================== */
-
-var map = L.map('map', {
-  center: [30.566174, -60.867132],
-  zoom: 3
-});
-// for storing sublayer outside of createlayer
-var sublayers;
-
-// // Add data layer to your map
-// cartodb.createLayer(map,layerSource)
-//     .addTo(map)
-//     .done(function(layer) {
-//         sublayer = layer.getSubLayer(0);
-//         createSelector(sublayer);
-//     })
-//     .error(function(err) {
-//         console.log("error: " + err);
-//     });
-
-var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-  attribution: 'Map tiles by <State`="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-  subdomains: 'abcd',
-  minZoom: 0,
-  maxZoom: 20,
-  ext: 'png'
-}).addTo(map);
+  var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+    attribution: 'Map tiles by <State`="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    subdomains: 'abcd',
+    minZoom: 0,
+    maxZoom: 20,
+    ext: 'png'
+  }).addTo(map);
 };
